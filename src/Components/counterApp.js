ef14e7c128c0454d68b8566bc8053f779887559f
Dustin Brinkman <mimirs.spring@gmail.com>
2021-08-01 12:21:57 -0400

    Work Done To Hero Cards

--- src/Components/counterApp.js
+++ src/Components/counterApp.js
@@ -6,22 +6,18 @@ import { setLockrCounter, saveLockr } from "../Hooks/hooks";
 let commaNumber = require("comma-number");
 
 //There's a lot going on here I'll walk you through
+
 const Counter = (props) => {
-  //1. We gotta destructure props so I dont have to type props.state. 
-  //Every letter saved counts!
   const { state, setState } = props;
-  //2. This state is so that when the big red button is pressed, it 
-  //changes the css class. It would be helpful for tracking how many 
-  //times a user has clicked the button as well if maybe in the future I 
-  //added leaderboards for that sort of thing
+  //1. We gotta destructure props so I dont have to type props.state.
+  //Every letter saved counts!
+  
   const [press, setPress] = useState(false);
-  //3. As it states, this is a function that runs when the app is first
-  //loaded. It does an api call to worldtimeapi.org to get unixtime. 
-  //Which is basically the amount of seconds that have past since some 
-  //time in 1970. I take that time, add 2 seconds to account for the 
-  //delay making the request, then subtract it by the last time stamp 
-  //saved in local storage. Then I take that result and multiply it by 
-  //the user's CPS (Counts Per Second)
+  //2. This state is so that when the big red button is pressed, it
+  //changes the css class. It would be helpful for tracking how many
+  //times a user has clicked the button as well if maybe in the future I
+  //added leaderboards for that sort of thing
+
   const onLoad = () => {
     axios
       .get("http://worldtimeapi.org/api/ip")
@@ -49,9 +45,14 @@ const Counter = (props) => {
         console.log(err.message);
       });
   };
-  //4. This is a function that keeps track of CPS (Counts Per Second)
-  //which means how many points the workers you bought from the shop are 
-  //getting you per second
+  //3. As it states, this is a function that runs when the app is first
+  //loaded. It does an api call to worldtimeapi.org to get unixtime.
+  //Which is basically the amount of seconds that have past since some
+  //time in 1970. I take that time, add 2 seconds to account for the
+  //delay making the request, then subtract it by the last time stamp
+  //saved in local storage. Then I take that result and multiply it by
+  //the user's CPS (Counts Per Second)
+
   const perSecond = () => {
     return (
       state.minions +
@@ -63,8 +64,10 @@ const Counter = (props) => {
       state.angels * 144
     );
   };
-  //5. This useEffect adds your CPS every second to the counter(big 
-  //numbers in the middle of the big red button)
+  //4. This is a function that keeps track of CPS (Counts Per Second)
+  //which means how many points the workers you bought from the shop are
+  //getting you per second
+
   useEffect(() => {
     const timer = setInterval(() => {
       setState((state) => ({
@@ -80,17 +83,15 @@ const Counter = (props) => {
           state.angels * 144,
       }));
     }, 1000);
+    //5. This useEffect adds your CPS every second to the counter(big
+    //numbers in the middle of the big red button)
 
     return () => {
       clearInterval(timer);
     };
     // eslint-disable-next-line
   }, []);
-  //6. This useEffect saves your progress every 2 seconds. It isn't every
-  // second because I felt it was unnecessary and it ran smoother doing 
-  //it every 2 seconds, not to mention it saves everytime state is 
-  //updated anyways. It's redundant to prevent users from changing 
-  //localstorage and cheating
+
   useEffect(() => {
     function sleep(ms) {
       return new Promise((resolve) => setTimeout(resolve, ms));
@@ -99,17 +100,12 @@ const Counter = (props) => {
       saveLockr(state);
     });
   }, [state]);
-  //7. Changes the class name when the big red button is pressed so that 
-  //it does the animation
-  const increased = () => {
-    if (press === true) {
-      return "increased";
-    } else {
-      return "increase";
-    }
-  };
-  //8. This is the actual onClick function that runs every time you press 
-  //the button
+  //6. This useEffect saves your progress every 2 seconds. It isn't every
+  // second because I felt it was unnecessary and it ran smoother doing
+  //it every 2 seconds, not to mention it saves everytime state is
+  //updated anyways. It's redundant to prevent users from changing
+  //localstorage and cheating
+
   const onClickIncrease = () => {
     setState((state) => ({
       ...state,
@@ -121,19 +117,20 @@ const Counter = (props) => {
       setPress(false);
     }, 1000);
   };
-  //9. It's our api call shoved into a useEffect! I did it this way so 
-  //that if I wanted to put the onLoad function in a seperate file at 
-  //some point, it would be easier.
+  //7. This is the actual onClick function that runs every time you press
+  //the button
+
   useEffect(() => {
     onLoad();
     // eslint-disable-next-line
   }, []);
+  //8. It's our api call shoved into a useEffect! I did it this way so
+  //that if I wanted to put the onLoad function in a seperate file at
+  //some point, it would be easier.
 
-  //10. Ahhhh finally the JSX. commaNumber is a dependancy I installed to 
-  //automatically put commas on numbers past 3 digits. It comes in handy!
   return (
     <div className="counter">
-      <button className={increased()} onClick={onClickIncrease}>
+      <button className={press === true ? "increased" : "increase"} onClick={onClickIncrease}>
         <span className="counterText">{commaNumber(state.counter)}</span>
         <p>CPS: {commaNumber(perSecond())}</p>
       </button>
@@ -141,5 +138,7 @@ const Counter = (props) => {
     </div>
   );
 };
+//9. Ahhhh finally the JSX. commaNumber is a dependancy I installed to
+//automatically put commas on numbers past 3 digits. It comes in handy!
 
 export default Counter;
--- src/Components/heroCards.js
+++ src/Components/heroCards.js
@@ -3,36 +3,52 @@ import Button from "@material-ui/core/Button";
 
 function HeroCards(props) {
   //arrays
+  const banana = (id) => { 
+  setHeroes((heroes) => 
+  heroes.map((hero) => {
+    return hero.id === id ? {
+      ...hero,
+      alive: "Dead",
+      disableBtn: true,
+    }
+    : hero
+  }))
+  }
   const heroArray = [
     {
       name: "Dookie Bird",
       health: 100,
       alive: "Alive",
       id: 1,
+      disableBtn: false,
     },
     {
       name: "Coffee Waffle",
       health: 100,
       alive: "Alive",
       id: 2,
+      disableBtn: false,
     },
     {
       name: "Jake From Statefarm",
       health: 100,
       alive: "Alive",
       id: 3,
+      disableBtn: false,
     },
     {
       name: "Jeff",
       health: 100,
       alive: "Alive",
       id: 4,
+      disableBtn: false,
     },
     {
       name: "The Milk Man",
       health: 100,
       alive: "Alive",
       id: 5,
+      disableBtn: false,
     },
   ];
   //state variables
@@ -53,8 +69,8 @@ function HeroCards(props) {
       {/* hero array being mapped */}
       {heroes.map((hero) => {
         return (
-          <div className="hero-list">
-            <Button variant="contained" color="secondary" size="large">
+          <div className="hero-list" key={hero.id}>
+            <Button variant="contained" disabled={hero.disableBtn} color="secondary" size="large" onClick={() => banana(hero.id)}>
               <p>{hero.name}</p>
               <p>{hero.health}</p>
               <p>{hero.alive}</p>
